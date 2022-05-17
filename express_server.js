8703834bbaae9db1fbd1e05305506a83d17d2404
Kevin Holmes <kevinholmes921@gmail.com>
2022-05-17 03:02:48 +0000

    function to save url to data base/redirecting to webpage

--- express_server.js
+++ express_server.js
@@ -13,12 +13,6 @@ const urlDatabase = {
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -42,8 +36,21 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.b2xVn2};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generaterRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`)
 });
 
 
@@ -58,5 +65,7 @@ function generaterRandomString() {
     string += chars.charAt(Math.floor(Math.random() * chars.length));
     string = string.slice(0, 6)
   }
-  console.log(string)
+  return string
 }
+
+generaterRandomString()
